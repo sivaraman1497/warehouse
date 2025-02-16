0cf2d2b1f720470efd3a1f127fc0e3d57aae2928
Siva Raman <sivaramantec21@gmail.com>
2025-02-16 23:34:47 +0530

    Created/modified dashboard components for Admin and user

---
+++ src/Components/AdminDashboard.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { useUser } from '../Custom/UserContext';    // custom hook
+
+function AdminDashboard(props) {
+    const navigate = useNavigate()
+
+    if(!props.username)
+    {
+        return null;
+    }
+
+    else
+    {
+        return (
+            <>
+                <WelcomeHeader username={props.username}/>
+                <HomePage/>
+                <LogoutButton navigate={navigate}/>
+            </>
+        );
+    }
+}
+
+function WelcomeHeader(username) {
+    // const { username, setUsername } = useUser()
+    return <h1>Welcome, {username.username}</h1>;
+}
+
+function HomePage() {
+    return (
+        <div className='row container'>
+            <div className='col-3'>
+                <Link className="btn btn-primary w-100" to="/categoriesList">Categories Management</Link>
+            </div>
+
+            <div className="col-3">
+                <Link className="btn btn-primary w-100" to="/itemsList">Items Management</Link>
+            </div>
+        </div>
+    );
+}
+
+const LogoutButton = React.memo(() => {
+    const navigate = useNavigate()
+
+    function handleLogout()
+    {
+        const url = 'http://localhost:5000/logout'
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include'
+        })
+        .then((res) => res.json())
+        .then(data => data.loggedout && navigate('/'))
+    }
+
+    return (
+        <div className='text-end'>
+            <button className='btn btn-primary' onClick={() => handleLogout()}>Logout</button>
+        </div>
+    )
+});
+
+// const MemoizedDashboard = React.memo(Dashboard)
+
+export default AdminDashboard
---
+++ src/Components/UserDashboard.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useNavigate, Link } from 'react-router-dom';
+
+function UserDashboard(props) {
+    const navigate = useNavigate()
+    return (
+        <>
+            <h1>Welcome, {props.username}</h1>
+            <OrdersManagement/>
+            <LogoutButton navigate={navigate}/>
+        </>
+    )
+}
+
+function OrdersManagement()
+{
+    return (
+        <>
+            <Link className="btn btn-primary" to="/neworder">Create new order</Link>
+        </>
+    )
+}
+
+const LogoutButton = React.memo(() => {
+    const navigate = useNavigate()
+
+    function handleLogout()
+    {
+        const url = 'http://localhost:5000/logout'
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include'
+        })
+        .then((res) => res.json())
+        .then(data => data.loggedout && navigate('/'))
+    }
+
+    return (
+        <div className='text-end'>
+            <button className='btn btn-primary' onClick={() => handleLogout()}>Logout</button>
+        </div>
+    )
+});
+
+export default UserDashboard
