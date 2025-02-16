c3811d018b996276602329531c2bbb3e474291b8
Siva Raman <sivaramantec21@gmail.com>
2025-02-16 23:35:35 +0530

    Code optimization/removed excess useState variables and code restructuring

--- src/Forms/LoginForm.jsx
+++ src/Forms/LoginForm.jsx
@@ -7,17 +7,14 @@ function LoginForm()
 {
     const [value, setValue] = useState({email:'', password:''});
     const [error, setError] = useState({emailError:'', passwordError:''});
-    const [dataError, setDataError] = useState('');
-    const [dataDb, setdataDb] = useState('');
     const { setUsername } = useUser();  // custom hook
 
     const navigate = useNavigate();
 
-    useEffect(() => {
-        if(dataError === 0)
-        {   
+    useEffect(() => { 
+        if(Object.keys(error).length === 0)
+        {
             const url = 'http://localhost:5000/verifyUser';
-
             fetch(url, {
                 method: 'POST',
                 headers: {
@@ -31,15 +28,17 @@ function LoginForm()
                 (data) => {
                     if(data.dataVal == 'success')
                     {
-                        setdataDb(data.dataVal)
-                        setUsername(data.firstname)
-                        navigate('/my'); 
+                        navigate('/my')
+                    }
+                    else
+                    {
+                        setError((prev) => ({...prev, dbError: ` - ${data.dataVal}`}))
                     }
                 }
             )
             .catch((err) => console.log(err))
         }
-    }, [error, dataError])
+    })
 
     useEffect(() => {
         const url = 'http://localhost:5000/loggedin';
@@ -58,43 +57,44 @@ function LoginForm()
     function handleSubmit(e)
     {
         e.preventDefault();
-
         let errCount = Object.keys(value).length;
 
         for(let key in value)
         {
-            let elementName = value[`${key}`];
+            let elementValue = value[`${key}`];
             let errorName = `${key}Error`;
 
-            if(elementName === '')
+            if(elementValue === '')
             {
                 setError((prev) => ({...prev, [errorName]:`Enter ${key}`}))
             }
 
             else
             {
-                setError((prev) => ({...prev, [errorName]:''}))
                 errCount-=1;
             }
         }
 
-        (errCount === 0) ? setDataError(0) : setDataError(1)
+        !errCount && setError({})
     }
 
     function handleInputChange(e)
     {
-        setValue((prev) => ({...prev, [e.target.name]:e.target.value}))       
+        if(e.target.value !== '')
+        {
+            setValue((prev) => ({...prev, [e.target.name] : e.target.value}))
+        }
     }
 
     return (
         <div className='container m-auto text-center w-25 mt-5'>
+            <div className='row container text-danger'>
+                {error.dbError || ''}
+            </div>
+
             <form method='POST' onSubmit={handleSubmit}>
                 <div>
-                {dataDb}
-                </div>
-
-                <div>
-                    <input type='text' className='form-control mt-2' name='email' id='email' autoComplete='off' placeholder='Enter email' onChange={handleInputChange} value={value.email}/>
+                    <input type='text' className='form-control mt-2' name='email' id='email' autoComplete='off' placeholder='Enter email' onChange={handleInputChange}/>
                     <div className='text-danger mt-2' id='emailError'>{error.emailError}</div>
                 </div>
                 <div>
